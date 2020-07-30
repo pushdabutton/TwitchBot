0f7ae564c45711262fab1dc948bfb0e3af8a5322
Alex Seant <ipushdabutton@gmail.com>
2020-07-30 14:09:02 -0400

    bug fix

---
+++ constants.js
@@ -0,0 +1,10 @@
+export const CHANNEL_NAME = '#yourChannelName';
+export const OAUTH_TOKEN = 'oauth:yourToken';
+export const BOT_USERNAME = 'yourChannelName';
+export const BLOCKED_WORDS = [
+    'cats',
+    'dogs',
+    'Fuck',
+    'Shit',
+    'Bitch'
+]
